ecf1f6d465837a22188af37a97ec0fac49b00667
Bochun Zhang <zbc0813@gmail.com>
2015-07-27 20:57:29 -0700

    ios app added

---
+++ CloudDriveMigrationServiceIOSClient/CloudDriveMigrationServiceIOSClient/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CloudDriveMigrationServiceIOSClient
+//
+//  Created by Bochun Zhang on 7/27/15.
+//  Copyright (c) 2015 Microsoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CloudDriveMigrationServiceIOSClient/CloudDriveMigrationServiceIOSClient/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CloudDriveMigrationServiceIOSClient
+//
+//  Created by Bochun Zhang on 7/27/15.
+//  Copyright (c) 2015 Microsoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
