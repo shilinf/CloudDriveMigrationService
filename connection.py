573059c22ab9e771f9b793791ad4112ecf4c6f3a
shilinfeng <shilinf@umich.edu>
2015-07-29 00:12:41 -0700

    file pull and push

---
+++ connection.py
@@ -0,0 +1,103 @@
+# Include the Dropbox SDK
+import dropbox
+import requests
+import json
+import re
+from urllib import urlencode, quote
+
+
+
+class MyFile:
+    def __init__(self, path, isFolder):
+        self.path = path
+        self.isFolder = isFolder
+
+
+class BaseDrive:
+    def __init__(self, token, rootPath):
+        self.token = token
+        self.rootPath = rootPath
+    def push(self, myfile, temp_filename):
+        return
+    def get(self, myfile, temp_filename):
+        return
+    def ls(self, path):
+        return
+
+
+class OneDrive(BaseDrive):
+    def __init__(self, token, rootPath):
+        BaseDrive.__init__(self, token, rootPath)
+    def push(self, myfile, temp_filename):
+        myUrl = 'https://api.onedrive.com/v1.0/drive/root:'+ r'/'.join([quote(s) for s in re.split(r'\/', myfile.path)])+':/content?'\
+                + urlencode({'access_token': self.token})
+        print myUrl
+        with open(temp_filename) as fh:
+            mydata = fh.read()
+            response = requests.put(myUrl, 
+                data=mydata,
+                )
+            print json.dumps(json.loads(response.content), indent = 2)
+
+
+
+class DropBox(BaseDrive):
+    def __init__(self, token, rootPath):
+        BaseDrive.__init__(self, token, rootPath)
+        self.client = dropbox.client.DropboxClient(token)
+    def ls(self, path):
+        folder_metadata = self.client.metadata(path)
+        #json_metadata = json.dumps(folder_metadata, indent=2)
+        contents = folder_metadata['contents']
+        files = []
+        for content in contents:
+            if content['is_dir']:
+                files.append(MyFile(content['path'], True))
+            else:
+                files.append(MyFile(content['path'], False))
+        return files
+    def get(self, myfile, temp_filename):
+        out = open(temp_filename, 'wb')
+        f = self.client.get_file(myfile.path)
+        out.write(f.read())
+        out.close()
+
+
+def slave(dropbox_token, onedrive_token, rootPath):
+    mydropbox = DropBox(dropbox_token, rootPath)
+    onedrive = OneDrive(onedrive_token, rootPath)
+    uploadFolder(mydropbox, onedrive, rootPath)
+
+
+
+def uploadFolder(mydropbox, onedrive, path):
+    files = mydropbox.ls(path)
+    for myfile in files:
+        if not myfile.isFolder:
+            mydropbox.get(myfile, 'temp')
+            onedrive.push(myfile, 'temp')
+        else:
+            uploadFolder(mydropbox, onedrive, myfile.path)
+
+
+
+
+slave('3ydFc4zVO4AAAAAAAAAABkzEqshhVNQv-C2LQu1Zx4mF4SVLq5rsiAzvvoxVbvju', 'EwB4Aq1DBAAUGCCXc8wU/zFu9QnLdZXy+YnElFkAATrELwlbY3t36BSlOr4PDLeh6Hr9axI9h1/ViNug90xFInI6B8blEcWu+NFypnHpbz4a/BbOzXD525pzUcKojUdPz9eQzVQA57RaAuozGwQD8VkgKbzAItNSp5lJ5TQJt0jP51tPWPiD7SXcHv0sOZ2tM1K7Hfoy+bOhaLPshlr4rgayik+AB5kEyHIM4hZS96MHqWGpTJ4WMhHJsjpWxSrntHOmAmVyclWjzNWRJFEn0IJ1hk5025oq2dZbwGosmKEesPTJhWevPjOkdFDRa1lJaKqzBjqGrTlYriAsKH/IJmu8uH1Tn4/pHAogK75FWG51rnc4EbN35dJcssmVTYcDZgAACMlAGRZJRBOPSAEOWMvQvwxJlSAdxAWQiZP1NotS0hWzvUlPevGbpnclaaye5Z4oVtemteJvTc1K15qT0zLz7gGkvuoMZkBrN0j4dAyHgDXpTRnXbgQZosoIZdf9VliTc5Xt9pNDEukiq/QXAc0KfG3wDgLiO8g1eQGFjiTktYq+/FaY+YiYMyVaZsrek4aXChGwNHxwrygUAi49mE8bgETciH4Z//fmodt7CP0oJ45tCtgVrmh1ZqFnVeC0yc544kBjOpRyPH972eJQ9SHSkHAEakR2QHv1lTWB2Q8Xn5vrN830zdok8B7KoCax75tSz3voACAs0ytO0sYahKFCMv3ygxmzoW29NzP1yqyY5OMipCQ0Ey/i7+wo4OMX2lB6nkwKO3NjeO9S0mZh8pDZfP0AVTZstGo/kBK5Myms8lyXw3x3rGcEjSdQGNw1WNf6TPy8YwE=', '/')
+
+
+'''
+client = dropbox.client.DropboxClient('3ydFc4zVO4AAAAAAAAAABkzEqshhVNQv-C2LQu1Zx4mF4SVLq5rsiAzvvoxVbvju')
+#print 'linked account: ', client.account_info()
+
+folder_metadata = client.metadata('/')
+print "metadata:", json.dumps(folder_metadata, indent=2)
+
+f, metadata = client.get_file_and_metadata('/Get Started with Dropbox.pdf')
+out = open('Dropbox.pdf', 'wb')
+out.write(f.read())
+out.close()
+'''
+
+
+
+
--- server.py
+++ server.py
@@ -5,32 +5,13 @@ import urllib
 import cgi
 import json
 import requests
+import connection
 
 PORT = 5050
 
-class File:
-    def __init__(self, name, relaPath, localName, isFolder):
-        self.name = name
-        self.relaPath = relaPath
-        self.localName = localName
-        self.isFolder = isFolder
-
-class BaseDrive:
-    def __init__(self, token):
-        self.token = token
-    def push(self, rootPath, file):
-        return
-
-    def get(self, rootPath, file):
-        return
-    def ls(self, rootPath, folder):
-        return
 
 
-class File:
-    def __init__(self, furl, fname):
-        self.url = furl
-        self.name = fname
+
 
 class HttpRequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
     def do_POST(self):
