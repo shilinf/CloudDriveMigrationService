c2d42c29c81584305bb4dfba725bee458e3dab0a
sushaoxiang911 <sushaoxiang911@SuShaoxiangs-MacBook-Pro.local>
2015-07-28 22:12:49 -0700

    server.py

--- server.py
+++ server.py
@@ -8,16 +8,29 @@ import requests
 
 PORT = 5050
 
+class File:
+    def __init__(self, name, relaPath, localName, isFolder):
+        self.name = name
+        self.relaPath = relaPath
+        self.localName = localName
+        self.isFolder = isFolder
+
+class BaseDrive:
+    def __init__(self, token):
+        self.token = token
+    def push(self, rootPath, file):
+        return
+
+    def get(self, rootPath, file):
+        return
+    def ls(self, rootPath, folder):
+        return
+
 
 class File:
     def __init__(self, furl, fname):
         self.url = furl
         self.name = fname
-
-    def download(self):
-        
-
-
 
 class HttpRequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
     def do_POST(self):
